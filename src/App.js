acbf97fd8fca749b3227e06681993c37285fa7f4
CodeChitra <codechitra@gmail.com>
2023-07-17 19:35:37 +0530

    Toggle sidebar done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
 
+import { Home, About, Cart, Products, SingleProduct, PrivateRoute, Error, Checkout } from "./pages";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path='/products' element={<Products />}></Route>
+        <Route path='/products/:id' element={<SingleProduct />}></Route>
+        <Route path='/checkout' element={<Checkout />}></Route>
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
